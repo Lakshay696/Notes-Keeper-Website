b55d44b60ee278d807e5f025aabde20540959bd5
lakshbansal41@gmail.com <lakshaybansal44@gmail.com>
2024-07-01 15:38:29 +0530

    first commit

---
+++ src/Components/Notes.jsx
@@ -0,0 +1,21 @@
+const notes = [
+    {
+        key: 1,
+        title:"Delegation",
+        content:"how many programmmers does it take to change a light bulb?"
+
+
+    },
+    {
+        key:2,
+        title:"Loops",
+        content:"How to keep a programmer in the shower forever.Show him the shampoo"
+    },
+    {
+        key:3,
+        title:"Arrays",
+        content:"why did the programmer quit his job.Show him the shampoo"
+    }
+]
+
+export default notes;
